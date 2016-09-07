0ce2bb790476a65c1a72e85fb12d5323125d2cb6
somesh <somesh.jagarapu@gmail.com>
2016-09-07 19:44:44 +0530

    Somesh changes

--- src/EpitaCourseBundle/Admin/CourseAdmin.php
+++ src/EpitaCourseBundle/Admin/CourseAdmin.php
@@ -6,27 +6,40 @@ use Sonata\AdminBundle\Admin\Admin;
 use Sonata\AdminBundle\Datagrid\ListMapper;
 use Sonata\AdminBundle\Datagrid\DatagridMapper;
 use Sonata\AdminBundle\Form\FormMapper;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 
 class CourseAdmin extends Admin
 {
     // Fields to be shown on create/edit forms
     protected function configureFormFields(FormMapper $formMapper)
     {
-        $formMapper
+        $formMapper  
             ->add('coursename', 'text', array('label' => 'Course Name'))
             ->add('teachingunit', 'text', array('label' => 'Teaching Unit'))
             ->add('coursecode', 'text', array('label' => 'Course Code'))
+            ->add('ectscredits','text', array('label' => 'ectscredits'))    
             ->add('atthours', 'text', array('label' => 'Hours'))
+            ->add('program', ChoiceType::class, array(
+               'choices'  => array('ME','MSc','ME and MSc',)))
+            ->add('specialization', ChoiceType::class, array(
+               'choices'  => array('FUND','FUND_HARMO','GITM','GITM_ISM','GITM_SDM_SNS','HARMO',
+                   'ISM_SE_CS','SDM','SDM_SE','SE','SNS','SNS_CSCS_SNS')))       
+            ->add('prerequistes','textarea', array('label' => 'prerequistes'))
+            ->add('learningoutcomes','textarea', array('label' => 'learningoutcomes')) 
+            ->add('courseoverview','textarea', array('label' => 'courseoverview'))
+            ->add('listoftopics','textarea', array('label' => 'listoftopics'))
+            ->add('assessmentscheme','textarea', array('label' => 'assessmentscheme'))      
+            ->add('recommendedresources','textarea', array('label' => 'recommendedresources'))       
         ;
     }
 
     // Fields to be shown on filter forms
     protected function configureDatagridFilters(DatagridMapper $datagridMapper)
     {
-        $datagridMapper
+        $datagridMapper  
             ->add('coursename')
             ->add('coursecode')
-            
+                    
         ;
     }
 
@@ -36,6 +49,7 @@ class CourseAdmin extends Admin
         $listMapper
             ->addIdentifier('coursename')
             ->addIdentifier('coursecode')
+            ->addIdentifier('teachingunit')  
         ;
     }
 }
